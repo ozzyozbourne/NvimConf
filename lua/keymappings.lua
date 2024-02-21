fb4dccc86a9a7e995ce1072e69993b4afb0f1fea
osaid khan <ozzy@osaids-Air.lan>
2024-02-21 00:16:25 -0500

    updated config

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -10,3 +10,6 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+--UndoTree
+vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, {})
