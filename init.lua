f044c2b693d3cca334e092ff43324dbf05c2890c
ozzyozbourne <khanosaid726@gmail.com>
2026-09-07 07:58:19 -0500

    bikeshedding

--- init.lua
+++ init.lua
@@ -1,9 +1,4 @@
-local ts_langs = {
-    "lua",  "python", "javascript", "typescript", 
-    "html", "css",    "json",       "bash", 
-    "zig",  "odin",   "c",          "cpp", 
-    "rust", "lean", 
-}
+local ts_langs = { "lua", "python", "javascript", "typescript", "html", "css", "json", "bash", "zig", "odin", "c",  "cpp", "rust" }
 
 vim.api.nvim_create_autocmd('PackChanged', {
   callback = function(ev)
@@ -15,12 +10,11 @@ vim.api.nvim_create_autocmd('PackChanged', {
   end
 })
 
-vim.api.nvim_create_autocmd("FileType", { pattern = ts_lang, callback = function() vim.treesitter.start() end })
+vim.api.nvim_create_autocmd("FileType", { pattern = ts_langs, callback = function() vim.treesitter.start() end })
 
 local b = 'https://github.com/'
 vim.pack.add({
   b .. 'nvim-treesitter/nvim-treesitter',
-  b .. 'nvim-tree/nvim-web-devicons',
   b .. 'dmtrKovalenko/fff',
 })
 require("nvim-treesitter").install(ts_langs)
@@ -44,11 +38,7 @@ vim.o.path = "**"
 vim.cmd("colorscheme retrobox")
 vim.cmd("command! -nargs=+ Grep execute 'silent grep! <args>' | copen")
 
-map("n", "<C-h>", "<C-w><C-h>")
-map("n", "<C-j>", "<C-w><C-j>")
-map("n", "<C-k>", "<C-w><C-k>")
-map("n", "<C-l>", "<C-w><C-l>")
-
+local map = vim.keymap.set
 map("n", "<C-d>", "<C-d>zz")
 map("n", "<C-u>", "<C-u>zz")
 
@@ -59,12 +49,5 @@ map("n", "n", "nzzzv")
 map("n", "N", "Nzzzv")
 map("v", "J", ":m '>+1<CR>gv=gv")
 map("v", "K", ":m '<-2<CR>gv=gv")
-map("t", "<ESC>", "<C-\\><C-n>")
 map("n", "<A-h>", ":below term<CR>i")
-
-map("n", "<leader>e", ":Ex<CR>")
-map("n", "<leader>v", ":edit $MYVIMRC<CR>")
-map("n", "<leader>r", ":make!<CR>")
-map("n", "<leader>R", ":set makeprg=")
-map("n", "<leader>x", ":copen<CR>")
-map("n", "<leader>c", ":!ctags -R .<CR>")
+map("t", "<ESC>", "<C-\\><C-n>")
