2db4809c6bdc3bc08ab06cc9129efc0c0c799317
ozzyozbourne <khanosaid726@gmail.com>
2026-09-07 06:04:44 -0500

    long time no see

--- init.lua
+++ init.lua
@@ -1,23 +1,29 @@
-local ts_langs = {"lua", "python", "javascript", "typescript", "html", "css", "json", "bash", "zig", "c", "cpp", "rust"}
+local ts_langs = {
+    "lua",  "python", "javascript", "typescript", 
+    "html", "css",    "json",       "bash", 
+    "zig",  "odin",   "c",          "cpp", 
+    "rust", "lean", 
+}
+
 vim.api.nvim_create_autocmd('PackChanged', {
   callback = function(ev)
-    if ev.data.spec.name == 'telescope-fzf-native.nvim' then
-      local kind = ev.data.kind
-      if kind == 'install' or kind == 'update' then vim.system({ 'make' }, { cwd = ev.data.path }) end
+    local d = ev.data
+    if d.spec.name == 'fff' and (d.kind == 'install' or d.kind == 'update') then
+        if not d.active then vim.cmd.packadd('fff') end 
+        require('fff.download').download_or_build_binary()
     end
   end
 })
-local base = 'https://github.com/'
+
+vim.api.nvim_create_autocmd("FileType", { pattern = ts_lang, callback = function() vim.treesitter.start() end })
+
+local b = 'https://github.com/'
 vim.pack.add({
-  base .. 'nvim-treesitter/nvim-treesitter',
-  base .. 'nvim-tree/nvim-web-devicons',
-  base .. 'nvim-telescope/telescope-fzf-native.nvim',
-  base .. 'nvim-lua/plenary.nvim',
-  base .. 'nvim-telescope/telescope.nvim',
+  b .. 'nvim-treesitter/nvim-treesitter',
+  b .. 'nvim-tree/nvim-web-devicons',
+  b .. 'dmtrKovalenko/fff',
 })
 require("nvim-treesitter").install(ts_langs)
-require('telescope').load_extension('fzf')
-
 
 vim.g.mapleader = " "
 vim.o.termguicolors = true
@@ -37,12 +43,7 @@ vim.o.list = true
 vim.o.path = "**"
 vim.cmd("colorscheme retrobox")
 vim.cmd("command! -nargs=+ Grep execute 'silent grep! <args>' | copen")
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "lua", "python", "javascript", "typescript", "html", "css", "json", "sh", "bash", "zig", "c", "cpp", "rust" },
-    callback = function() vim.treesitter.start() end
-})
 
-local map, builtin = vim.keymap.set, require("telescope.builtin")
 map("n", "<C-h>", "<C-w><C-h>")
 map("n", "<C-j>", "<C-w><C-j>")
 map("n", "<C-k>", "<C-w><C-k>")
@@ -61,15 +62,8 @@ map("v", "K", ":m '<-2<CR>gv=gv")
 map("t", "<ESC>", "<C-\\><C-n>")
 map("n", "<A-h>", ":below term<CR>i")
 
-map("n", "<leader>w", ":write<CR>")
-map("n", "<leader>q", ":quit<CR>")
-map("n", "<leader>Q", ":quit!<CR>")
 map("n", "<leader>e", ":Ex<CR>")
 map("n", "<leader>v", ":edit $MYVIMRC<CR>")
-map('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-map('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-map('n', '<leader>fb', builtin.buffers, { desc = 'Telescope require("telescope").buffers' })
-map('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 map("n", "<leader>r", ":make!<CR>")
 map("n", "<leader>R", ":set makeprg=")
 map("n", "<leader>x", ":copen<CR>")
